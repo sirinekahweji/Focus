24d4f0755f2b25fd954aa92b567ef447ac9800f2
Sirine Kahweji <sirinekahweji5@gmail.com>
2025-08-22 21:10:29 +0200

    reglage probleme roting et install Clerk

--- focus/src/app/app.routes.ts
+++ focus/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 export const routes: Routes = [
-      { path: '', component: HomeComponent }
+      { path: '', component: HomeComponent },
+      { path: 'signin', component: SigninComponent },
+      { path: 'signup', component: SignupComponent }
 
 ];
+
+
