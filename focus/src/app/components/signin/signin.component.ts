f335f51a9598d47b0cf97f1d442c8c462f69e78f
Sirine Kahweji <sirinekahweji5@gmail.com>
2025-08-23 15:09:12 +0200

    login page done

--- focus/src/app/components/signin/signin.component.ts
+++ focus/src/app/components/signin/signin.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component ,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+
+
+
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent,FormsModule],
   templateUrl: './signin.component.html',
-  styleUrl: './signin.component.css'
+  styleUrl: './signin.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]  
+
 })
 export class SigninComponent {
+  email = '';
+  password = '';
+
+  onSubmit() {
+    if(this.email === 'test@example.com' && this.password === '123456') {
+  console.log(`${this.email} + ${this.password}`);
+    } else {
+        console.log(` else hh ${this.email} + ${this.password}`);
+
+    }
+  }
 
 }
--- focus/src/main.ts
+++ focus/src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Clerk } from "@clerk/clerk-js";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const clerk = new Clerk("pk_test_am9pbnQtbWFzdGlmZi0xOS5jbGVyay5hY2NvdW50cy5kZXYk");
+
+clerk.load()
+  .then(() => {
+    console.log("Clerk chargé !");
+    bootstrapApplication(AppComponent, appConfig)
+      .catch(err => console.error(err));
+  })
+  .catch(err => console.error("Erreur Clerk:", err));
+
+export { clerk };
