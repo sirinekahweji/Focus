cfcda9b7acb8aea939eab85b3af656863e409cf8
Sirine Kahweji <sirinekahweji5@gmail.com>
2025-08-24 16:09:14 +0200

    sign up

--- focus/src/app/components/signup/signup.component.ts
+++ focus/src/app/components/signup/signup.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,NavbarComponent],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+
+   email = '';
+  password = '';
+
+  onSubmit() {
+    if(this.email === 'test@example.com' && this.password === '123456') {
+  console.log(`${this.email} + ${this.password}`);
+    } else {
+        console.log(` else hh ${this.email} + ${this.password}`);
+
+    }
+  }
 
 }
